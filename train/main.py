e003272b75949d0faf49895a142b475850ed09ec
SabaPivot <careforme.dropout@gmail.com>
2024-12-08 16:47:15 +0000

    Add: mode between tran/inference

--- train/main.py
+++ train/main.py
@@ -1,4 +1,4 @@
-import os, yaml
+import os, yaml, argparse
 import torch
 from model import load_peft_model
 from data import load_train_data_in_chat_template, load_test_data_in_chat_template
@@ -10,9 +10,6 @@ with open("trainer.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 def main(mode):
-    
-
-if __name__ == "__main__":
     # Load model with Unsloth
     model_name = config["model"]["model_path"]
     model, tokenizer = load_peft_model(model_name)
@@ -20,13 +17,20 @@ if __name__ == "__main__":
     # Load Datasets
     data_path, domain = "HAERAE-HUB/KMMLU", config["dataset"]["domain"]
 
+    # Load data in chat template
     train_data = load_train_data_in_chat_template(data_path, domain, tokenizer)
-    dev_data, train_data = load_test_data_in_chat_template(data_path, domain, tokenizer)
+    dev_data, test_data = load_test_data_in_chat_template(data_path, domain, tokenizer)
 
-    # inference
-    inference(model, tokenizer, dev_data)
-    exit()
+    if mode == "train":
+        trainer = set_trainer(model, tokenizer, train_data)
+        trainer.train()
+    
+    elif mode == "inference":
+        inference(model, tokenizer, dev_data)
 
-    # Start Train
-    trainer = set_trainer(model, tokenizer, train_data)
-    trainer.train()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="KMMLU Train/Inference Script")
+    parser.add_argument("--mode", choices=["train", "inference"], required=True, help="Mode to run: 'train' or 'inference'")
+    args = parser.parse_args()
+
+    main(args.mode)
