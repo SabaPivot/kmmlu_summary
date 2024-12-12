2a7777ce64a9e642f26492d1f3f56831fbfe1eb2
SabaPivot <careforme.dropout@gmail.com>
2024-12-12 09:29:18 +0000

    Feature: execute the main.py with --fewshot to apply direct 5-shot, give nothing but only --mode to apply 0-shot

--- train/main.py
+++ train/main.py
@@ -9,7 +9,7 @@ os.environ["WANDB_PROJECT"] = "KMMLU"
 with open("trainer.yaml", "r") as file:
     config = yaml.safe_load(file)
 
-def main(mode):
+def main(mode, fewshot):
     # Model name
     model_name = config["model"]["model_path"]
 
@@ -26,12 +26,13 @@ def main(mode):
     
     elif mode == "inference":
         model, tokenizer = load_model(model_name)
-        test_data = load_test_data_in_chat_template(tokenizer, fewshot=False)
+        test_data = load_test_data_in_chat_template(tokenizer, fewshot=fewshot)
         inference(model, tokenizer, test_data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="KMMLU Train/Inference Script")
     parser.add_argument("--mode", choices=["train", "inference"], required=True, help="Mode to run: 'train' or 'inference'")
+    parser.add_argument("--fewshot", default=False, action='store_true', help="if type ""--fewshot"", do fewshot with dev data, if nothing, 0-shot")
     args = parser.parse_args()
 
-    main(args.mode)
+    main(args.mode, args.fewshot)
